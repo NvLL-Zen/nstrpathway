b038a6c0afa510b6f70cb56cbe58849913bdbfda
NvLL-Zen <nvll.zen@gmail.com>
2025-12-15 03:01:56 +0700

    V0.1

---
+++ src/components/pageSection.svelte
@@ -0,0 +1,15 @@
+<div class="pageSection">
+    <slot>
+
+    </slot>
+</div>
+
+<style>
+    .pageSection {
+        width: 100vw;
+        height: 100vh;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+    }
+</style>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,40 @@
+<script>
+    import '../styles/global.css'
+    import PageSection from '../components/pageSection.svelte';
+    
+    
+    import Slide_1 from '../../src/figmaAssets/Slide 4_3-1.svg';
+    import Slide_2 from '../../src/figmaAssets/Slide 4_3-2.svg';
+    import Slide_6 from '../../src/figmaAssets/Slide 4_3-6.svg';
+    import Slide_7 from '../../src/figmaAssets/Slide 4_3-7.svg';
+    import Slide_8 from '../../src/figmaAssets/Slide 4_3-8.svg';
+    import Slide_9 from '../../src/figmaAssets/Slide 4_3-9.svg';
+    import Slide_10 from '../../src/figmaAssets/Slide 4_3-10.svg';
+    import Slide_11 from '../../src/figmaAssets/Slide 4_3-11.svg';
+    import Slide_12 from '../../src/figmaAssets/Slide 4_3-12.svg';
+    import Slide_13 from '../../src/figmaAssets/Slide 4_3-13.svg';
+    import Slide_14 from '../../src/figmaAssets/Slide 4_3-14.svg';
+    import Slide_15 from '../../src/figmaAssets/Slide 4_3-15.svg';
+    import Slide_16 from '../../src/figmaAssets/Slide 4_3-16.svg';
+
+</script>
+
+<PageSection>
+    <img src={Slide_1} alt="idk">
+</PageSection>
+<PageSection>
+    <img src={Slide_2} alt="idk">
+</PageSection>
+<PageSection>
+    <img src={Slide_6} alt="idk">
+</PageSection>
+<PageSection>
+    <img src={Slide_7} alt="idk">
+</PageSection>
+<PageSection>
+    <img src={Slide_8} alt="idk">
+</PageSection>
+<PageSection>
+    <img src={Slide_9} alt="idk">
+</PageSection>
+
